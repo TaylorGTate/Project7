3d8fbddf8776e2f0c38ed2e45099184e72b48afe
taylortate4 <tgtate@valdosta.edu>
2019-01-28 22:15:56 -0500

    Added the ablity to upload a picture from a local file system onto the website as a book cover.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root 'store#index', as: 'store_index'
-
+#  root to: redirect('/products')
   resources :products
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
