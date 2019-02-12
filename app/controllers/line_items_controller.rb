f9997ac071fd5e9a3a230095b5663894ed89db71
taylortate4 <tgtate@valdosta.edu>
2019-02-11 19:36:58 -0500

    Finished project4 part1. Made it to were the customer can only access his or her shopping cart.

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -81,6 +81,9 @@ class LineItemsController < ApplicationController
     # Use callbacks to share common setup or constraints between actions.
     def set_line_item
       @line_item = LineItem.find(params[:id])
+			if @cart.id != session[:cart_id]
+      	invalid_cart
+   		end
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
