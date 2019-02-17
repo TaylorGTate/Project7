051e96f019ee55c919d497de49341f4ed11a49bf
taylortate4 <tgtate@valdosta.edu>
2019-02-17 17:38:10 -0500

    Completed iterations F1 through F4 in the book.

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -43,6 +43,7 @@ class LineItemsController < ApplicationController
     respond_to do |format|
       if @line_item.save
         format.html { redirect_to store_index_url }
+        format.js   { @current_item = @line_item }
         format.json { redirect_to cart_path(@line_item.cart)} 
       else
         format.html { render :new }
