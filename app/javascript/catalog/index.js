684bbb3a4e1c88585935dca7e90d5706cc7efb23
taylortate4 <tgtate@valdosta.edu>
2019-03-14 13:39:35 -0400

    fixing part 3

--- app/javascript/catalog/index.js
+++ app/javascript/catalog/index.js
@@ -4,7 +4,8 @@ import App from './routes';
 
 document.addEventListener("DOMContentLoaded", () => {
     const catalog = document.querySelector("#catalog");
+    const pay_type = JSON.parse(catalog.getAttribute("pay_type"));
     const seller = JSON.parse(catalog.getAttribute("seller"));
     const cart_id = JSON.parse(catalog.getAttribute("cart_id"));
-    ReactDOM.render(<App cart_id={cart_id} seller={seller} />, catalog);
+    ReactDOM.render(<App cart_id={cart_id} seller={seller} pay_type={pay_type} />, catalog);
 });
